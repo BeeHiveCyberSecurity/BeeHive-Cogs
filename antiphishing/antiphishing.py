62eb14e2147f76181fade045242d355d51cfe075
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-04-13 19:34:55 -0600

    Update antiphishing.py

--- antiphishing/antiphishing.py
+++ antiphishing/antiphishing.py
@@ -296,7 +296,7 @@ class AntiPhishing(commands.Cog):
             return
 
         await self.config.guild(ctx.guild).action.set(action)
-        embed = discord.Embed(title='Settings changed', description=f"Malicious links will now trigger an **`{action}`** event when detected in chat.\nYou can make changes to this setting at any time.", colour=2866574,)
+        embed = discord.Embed(title='Settings changed', description=f"Malicious links will now trigger a **`{action}`** event when detected in chat.\n\nYou can make changes to this setting at any time.", colour=2866574,)
         embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Green/checkmark-circle-outline.png")
         await ctx.send(embed=embed)
 
