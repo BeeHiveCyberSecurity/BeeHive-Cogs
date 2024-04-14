b8d74d68a92fd953950cd620c024a3681994e781
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-04-14 01:44:48 -0600

    Update __init__.py

--- malcore/__init__.py
+++ malcore/__init__.py
@@ -8,4 +8,4 @@ async def setup(bot: Red):
     await bot.add_cog(cog)
 
 
-__red_end_user_data_statement__ = "The Malcore cog by BeeHive does not store any user data. Samples shared with Malcore are kept public unless the user participating, and their API Key, are enrolled in the Malcore Signature Research Program."
+__red_end_user_data_statement__ = "This cog does not store any user data. Samples shared with Malcore are kept public unless the user participating, and their API Key, are enrolled in the Malcore Signature Research Program."
