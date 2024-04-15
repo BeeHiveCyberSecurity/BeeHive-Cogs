01a6c733d49906492c5f851b2642c87db852e28d
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-04-14 18:53:24 -0600

    Update malcore.py

--- malcore/malcore.py
+++ malcore/malcore.py
@@ -3,15 +3,16 @@ import re
 import json
 import discord
 from redbot.core import commands
+from redbot.core import app_commands
 
 class Malcore(commands.Cog):
-    """malcore file upload and analysis via Discord"""
+    """Malcore file upload and analysis via Discord"""
 
     def __init__(self, bot):
         self.bot = bot
         
-    @commands.command()
-    async def malcore(self, ctx, url: str):
+    @commands.hybrid_command(name="urlcheck", description="Submit content to Malcore for deep analysis")
+    async def urlcheck(self, ctx, url: str):
         mcore_key = await self.bot.get_shared_api_tokens("malcore")
         if mcore_key.get("api_key") is None:
             return await ctx.send("The Malcore API key has not been set.")
@@ -33,9 +34,9 @@ class Malcore(commands.Cog):
                 threat_level = json_data.get("data", {}).get("data", {}).get("threat_level")
                 embed = discord.Embed(url=f"{url}")
                 if threat_level and "SAFE" in threat_level:
-                    embed.title = f"That file looks safe!"
+                    embed.title = f"{url} looks safe"
                     embed.color = 0x2BBD8E  # Green color
-                    embed.description = f"There's nothing obviously malicious about this URL - it should be safe."
+                    embed.description = f"Malcore did not detect any threats associated with this URL"
                     embed.add_field(name="Overall verdict", value="Scanned and found safe", inline=False)
                     embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Green/checkmark-circle-outline.png")
                 else:
