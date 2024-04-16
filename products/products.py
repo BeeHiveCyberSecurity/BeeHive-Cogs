2e6ad74b973bcaaea4207bbfef8dcb30d78acb4c
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-04-16 12:43:48 -0600

    Update products.py

--- products/products.py
+++ products/products.py
@@ -7,8 +7,6 @@ class Products(commands.Cog):
     @commands.hybrid_group(name="product")
     async def product(self, ctx: commands.Context):
         await ctx.send("What would you like to learn more about?")
-        # As discussed above, top level hybrid command groups cannot be invoked as a slash command.
-        # Thus, this will not work as a slash command.
 
     @product.command(name="antivirus")
     async def bark(self, ctx: commands.Context):
