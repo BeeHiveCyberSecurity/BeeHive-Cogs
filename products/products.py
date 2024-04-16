0859d3369d9db1232006bf8b348df27e494ed6db
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-04-16 13:00:22 -0600

    Update products.py

--- products/products.py
+++ products/products.py
@@ -4,10 +4,12 @@ class Products(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @product.command(name="antivirus")
+    @commands.bot_has_permissions(embed_links=True)
+    @commands.hybrid_command(name="antivirus", description="Learn more about BeeHive's AntiVirus", aliases=["av"])
     async def antivirus(self, ctx: commands.Context):
         await ctx.send("antivirus test message lol")
     
-    @product.command(name="vulnerabilityscanning")
+    @commands.bot_has_permissions(embed_links=True)
+    @commands.hybrid_command(name="vulnerabilityscanning", description="Learn more about Vulnerability Scanning", aliases=[""])
     async def vulnerabilityscanning(self, ctx: commands.Context):
         await ctx.send("vuln scanning test message")
