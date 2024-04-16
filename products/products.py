2006ee2bcbdce2b1238fce0331f9d9f54cc50294
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-04-16 17:12:42 -0600

    Update products.py

--- products/products.py
+++ products/products.py
@@ -21,9 +21,10 @@ class Products(commands.Cog):
         Learn more [here](<https://www.beehive.systems/antivirus>)
         """
 
-        embed = discord.Embed(title=f"Award-winning protection against advanced online threats", description=f"# Protect your PC from malware and spyware in just a few clicks\n\nBeeHive's security client is a security software application designed to protect users from malware or viruses while working, shopping, or playing games on their computers. It works by isolating unknown files in a safe virtual environment before performing real-time analysis to determine whether they pose any threat - all done without risk or alert fatigue for normal computer usage.", colour=16767334, url='https://www.beehive.systems/antivirus')
+        embed = discord.Embed(title=f"Award-winning protection against advanced online threats", description=f"# Protect your PC from malware and spyware in just a few clicks\n\n**BeeHive's security client is a security software application designed to protect users from malware or viruses while working, shopping, or playing games on their computers. It works by isolating unknown files in a safe virtual environment before performing real-time analysis to determine whether they pose any threat - all done without risk or alert fatigue for normal computer usage.**", colour=16767334, url='https://www.beehive.systems/antivirus')
         embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Yellow/shield-checkmark.png")
         embed.set_image(url="https://i.imgur.com/NvGx5WK.png")
+        embed.add_field(name="​", value="​", inline=False)
         embed.add_field(name="Real-Time AntiMalware", value="High-sensitivity file inspection occurs on-device to detect and block known and unknown malicious software", inline=False)
         embed.add_field(name="Real-Time AntiSpyware", value="Powerful protection from credential stealers, banking trojans, and other hard-to-detect infections", inline=False)
         embed.add_field(name="Intrusion Detection and Prevention", value="Analysis and blocking of potentially dangerous activities on the host device to preserve system integrity", inline=False)
