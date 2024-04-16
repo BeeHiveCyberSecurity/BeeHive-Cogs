7a46e83883e15614d486a960c9dd9289baacc5da
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-04-16 13:21:23 -0600

    Update products.py

--- products/products.py
+++ products/products.py
@@ -1,6 +1,5 @@
 from redbot.core import commands
-from discord_slash.utils.manage_components import create_button, create_actionrow
-from discord_slash.model import ButtonStyle
+
 
 class Products(commands.Cog):
     def __init__(self, bot):
@@ -9,14 +8,7 @@ class Products(commands.Cog):
     @commands.bot_has_permissions(embed_links=True)
     @commands.hybrid_command(name="antivirus", description="Learn more about BeeHive's AntiVirus", aliases=["av"])
     async def antivirus(self, ctx: commands.Context):
-        buttons = [
-            create_button(
-                style=ButtonStyle.green,
-                label="A Green Button"
-            ),
-          ]
-        action_row = create_actionrow(*buttons)
-        await ctx.send("antivirus test message lol", components=[action_row])
+        await ctx.send("antivirus test message lol")
     
     @commands.bot_has_permissions(embed_links=True)
     @commands.hybrid_command(name="vulnerabilityscanning", description="Learn more about Vulnerability Scanning", aliases=[""])
