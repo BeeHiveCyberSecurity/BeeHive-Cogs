dcc7ec05a37c2567b1ce88d6cffbfbadfaa2635a
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-03 09:16:36 -0600

    Update stripeidentity.py

--- stripeidentity/stripeidentity.py
+++ stripeidentity/stripeidentity.py
@@ -107,16 +107,17 @@ class StripeIdentity(commands.Cog):
                 title="Age verification requested",
                 description=(
                     f"Hello {user.mention},\n\n"
-                    "Enhancing user safety bilaterally on Discord is a priority within our services and communities."
-                    "As part of our ongoing safety and security operations, your account has been selected to verify your age due to recent activity in one or more servers."
-                    "\n"
+                    "Enhancing user safety bilaterally on Discord is a priority within our services and communities. "
+                    "As part of our ongoing safety and security operations, your account has been selected to verify your age due to recent activity in one or more servers. "
+                    "\n\n"
                     "You have 15 minutes to complete this process. If you do not complete verification, you will be removed from the server for safety."
                 ),
-                color=discord.Color(0xff4545)
+                color=discord.Color(0xff4545),
+                thumbnail="https://www.beehive.systems/hubfs/Icon%20Packs/Red/id-card-sharp.png"
             )
             view = discord.ui.View()
             view.add_item(discord.ui.Button(label="Start verification", url=f"{verification_session.url}", style=discord.ButtonStyle.link, emoji="<:shield:1194906995036262420>"))
-            dm_message = await user.send(embed=dm_embed)
+            dm_message = await user.send(embed=dm_embed, view=view)
             embed = discord.Embed(description=f"Verification session created for {user.display_name}. Instructions have been sent via DM.", color=discord.Color.green())
             await ctx.send(embed=embed)
 
