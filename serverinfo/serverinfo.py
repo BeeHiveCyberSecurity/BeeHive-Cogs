3aad35ecc283239ba4af90328a56ddd419ae5e8b
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-08 23:04:44 -0600

    Update serverinfo.py

--- serverinfo/serverinfo.py
+++ serverinfo/serverinfo.py
@@ -268,11 +268,13 @@ class ServerInfoCog(commands.Cog):
                 self.add_item(Button(style=ButtonStyle.secondary, label="Previous", custom_id="previous"))
                 self.add_item(Button(style=ButtonStyle.secondary, label="Next", custom_id="next"))
 
-                    @discord.ui.button(label='Previous', style=discord.ButtonStyle.secondary)
-                    async def previous_button(self, button: discord.ui.Button, interaction: discord.Interaction):
+            @discord.ui.button(label='Previous', style=discord.ButtonStyle.secondary)
+            async def previous_button(self, button: discord.ui.Button, interaction: discord.Interaction):
+                pass
 
-                    @discord.ui.button(label='Next', style=discord.ButtonStyle.secondary)
-                    async def next_button(self, button: discord.ui.Button, interaction: discord.Interaction):
+            @discord.ui.button(label='Next', style=discord.ButtonStyle.secondary)
+            async def next_button(self, button: discord.ui.Button, interaction: discord.Interaction):
+                pass
 
         view = NavigationView()
         await ctx.send(embed=pages[0], view=view)
