3c90d11d43c9291559db51f4ee0878eed23924e6
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-08 23:40:49 -0600

    Update serverinfo.py

--- serverinfo/serverinfo.py
+++ serverinfo/serverinfo.py
@@ -263,7 +263,7 @@ class ServerInfoCog(commands.Cog):
 
         # Create a View with interaction buttons for navigation
         class NavigationView(View):
-            def __init__(self)
+            def __init__(self):
 
             @discord.ui.button(label="Previous", custom_id="previous_button_id")
             async def previous_button(self, button: discord.ui.Button, interaction: discord.Interaction):
