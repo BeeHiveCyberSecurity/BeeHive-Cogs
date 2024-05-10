57327279ed6296359e4418e8ed8d6d86d925058f
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-10 15:54:47 -0600

    Update skysearch.py

--- skysearch/skysearch.py
+++ skysearch/skysearch.py
@@ -598,8 +598,8 @@ class Skysearch(commands.Cog):
     @aircraft_group.command(name='listalertchannels', help='List the alert channels and their statuses.')
     async def list_alert_channels(self, ctx):
         guilds = self.bot.guilds
-        embed = discord.Embed(title="Alert Channels Status", color=0xfffffe)
         for guild in guilds:
+            embed = discord.Embed(title=f"Alert Channels Status for {guild.name}", color=0xfffffe)
             alert_channel_id = await self.config.guild(guild).alert_channel()
             if alert_channel_id:
                 alert_channel = self.bot.get_channel(alert_channel_id)
@@ -607,12 +607,12 @@ class Skysearch(commands.Cog):
                     next_iteration = self.check_emergency_squawks.next_iteration
                     time_remaining = next_iteration - datetime.datetime.now() if next_iteration else "Not scheduled"
                     last_check_status = "Successful, squawks found" if self.check_emergency_squawks.is_running() else "Successful, no squawks"
-                    embed.add_field(name=f"Guild: {guild.name}, Channel: {alert_channel.name}", value=f"Time until next check: {time_remaining}\nLast check status: {last_check_status}", inline=False)
+                    embed.add_field(name=f"Channel: {alert_channel.name}", value=f"Time until next check: {time_remaining}\nLast check status: {last_check_status}", inline=False)
                 else:
-                    embed.add_field(name=f"Guild: {guild.name}", value="No alert channel set.", inline=False)
+                    embed.add_field(name="Status", value="No alert channel set.", inline=False)
             else:
-                embed.add_field(name=f"Guild: {guild.name}", value="No alert channel set.", inline=False)
-        await ctx.send(embed=embed)
+                embed.add_field(name="Status", value="No alert channel set.", inline=False)
+            await ctx.send(embed=embed)
         
     @aircraft_group.command(name='alertchannel', help='Set a channel to send emergency squawk alerts to.')
     async def set_alert_channel(self, ctx, channel: discord.TextChannel):
