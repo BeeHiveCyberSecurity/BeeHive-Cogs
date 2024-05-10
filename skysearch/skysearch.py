f96a4b9e88ac7d0f9a93a970d6cb55101ff492e5
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-10 17:27:06 -0600

    Update skysearch.py

--- skysearch/skysearch.py
+++ skysearch/skysearch.py
@@ -66,7 +66,17 @@ class Skysearch(commands.Cog):
             else:
                 altitude_feet = f"{altitude} feet"
                 embed.add_field(name="Altitude", value=f"`{altitude_feet}`", inline=True)
-            embed.add_field(name="Heading", value=f"{aircraft_data.get('true_heading', 'N/A')} degrees", inline=True)
+            heading = aircraft_data.get('true_heading', None)
+            if heading is not None:
+                if 0 <= heading < 90:
+                    emoji = ":arrow_upper_right:"
+                elif 90 <= heading < 180:
+                    emoji = ":arrow_down_right:"
+                elif 180 <= heading < 270:
+                    emoji = ":arrow_down_left:"
+                else:
+                    emoji = ":arrow_upper_left:"
+                embed.add_field(name="Heading", value=f"{emoji} {heading} degrees", inline=True)
             embed.add_field(name="Position", value=f"{aircraft_data.get('lat', 'N/A')}, {aircraft_data.get('lon', 'N/A')}", inline=True)
             embed.add_field(name="Squawk", value=aircraft_data.get('squawk', 'N/A'), inline=True)
             embed.add_field(name="Emergency", value=emergency_status, inline=True)
