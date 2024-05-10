f29db9b429a7ec87f7e7ddf18a973628269979f3
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-09 20:15:23 -0600

    Update skysearch.py

--- skysearch/skysearch.py
+++ skysearch/skysearch.py
@@ -311,12 +311,15 @@ class Skysearch(commands.Cog):
             if len(response['ac']) > 1:
                 aircraft_list = []
                 for aircraft in response['ac'][:10]:  # Limit to first 10 results
-                    aircraft_type = aircraft.get('type', 'N/A')  # Aircraft Type
+                    aircraft_data = aircraft.get('desc', 'N/A')  # Aircraft Data
                     aircraft_icao = aircraft.get('icao', 'N/A')  # ICAO
                     aircraft_squawk = aircraft.get('squawk', 'N/A')  # Squawk
                     aircraft_altitude = aircraft.get('alt', 'N/A')  # Altitude
+                    aircraft_type = f"{aircraft_data} ({aircraft.get('t', 'N/A')})"  # Aircraft Type
                     aircraft_list.append(f"Aircraft Type: {aircraft_type}, ICAO: {aircraft_icao}, Squawk: {aircraft_squawk}, Altitude: {aircraft_altitude}")
-                embed = discord.Embed(title="Military Aircraft", description="\n".join(aircraft_list), color=0x00ff00)
+                embed = discord.Embed(title="Military Aircraft", color=0x00ff00)
+                for aircraft_info in aircraft_list:
+                    embed.add_field(name="Aircraft Info", value=aircraft_info, inline=False)
                 await ctx.send(embed=embed)
             else:
                 await self._send_aircraft_info(ctx, response)
