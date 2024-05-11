92cd4cddc048c2a174bdb94a05b5ea967ece0be4
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-11 03:50:37 -0600

    Update icao_codes.py

--- skysearch/icao_codes.py
+++ skysearch/icao_codes.py
@@ -1,6 +1,6 @@
 law_enforcement_icao_set = {'A10941', 'AB68C8', 'A9A449', 'A67117', 'A03325', 'A1B4BB', 'A2C934', 'AD933E', 'AC6E92', 'A64D15', 'A6A665', 'A97B90', 'A7C42B', 'A92BFF', 'ACD25B'}  # Update this set as needed or discovered
 
-military_icao_set = {'AE055F', 'AE0270', 'AE1462', 'AE146E', 'AE0664', 'AE4E0D', 'AE0808', 'AE0800', 'AE146B', 'AE4D69'}
+military_icao_set = {'AE055F', 'AE0270', 'AE1462', 'AE146E', 'AE0664', 'AE4E0D', 'AE0808', 'AE0800', 'AE146B', 'AE4D69', '48B12B'}
 
 medical_icao_set = {'A5C4D6', 'A234DB', 'A22B87', 'A740F0'}
 
