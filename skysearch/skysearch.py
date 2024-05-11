5b455fea174c108093a89f9925828be16313e0f6
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-11 17:52:48 -0600

    Tweet changes

--- skysearch/skysearch.py
+++ skysearch/skysearch.py
@@ -167,7 +167,7 @@ class Skysearch(commands.Cog):
             view = discord.ui.View()
             view.add_item(discord.ui.Button(label=f"Track live", url=f"{link}", style=discord.ButtonStyle.link))
             ground_speed_mph = ground_speed_mph if 'ground_speed_mph' in locals() else 'UNKNOWN'
-            tweet_text = f"Tracking flight {aircraft_data.get('flight', '')} at position {lat}, {lon} with speed {ground_speed_mph} mph using #SkySearch by @BeeHiveCyberSec. Track planes socially @ https://go.beehive.systems/discord"
+            tweet_text = f"Tracking flight {aircraft_data.get('flight', '')} at position {lat}, {lon} with speed {ground_speed_mph} mph using #SkySearch\n\nJoin via Discord to track planes with your friends for free - https://discord.gg/X8huyaeXrA"
             tweet_url = f"https://twitter.com/intent/tweet?text={urllib.parse.quote_plus(tweet_text)}"
             view.add_item(discord.ui.Button(label=f"Share on 𝕏", url=tweet_url, style=discord.ButtonStyle.link))
             await ctx.send(embed=embed, view=view)
