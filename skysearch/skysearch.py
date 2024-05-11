6b275b46c5dc39f490375c0951837f160efeba3d
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-11 02:36:26 -0600

    Updates

--- skysearch/skysearch.py
+++ skysearch/skysearch.py
@@ -16,10 +16,9 @@ from reportlab.pdfgen import canvas #type: ignore
 from reportlab.lib import colors#type: ignore
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle #type: ignore
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, PageBreak, Table, TableStyle #type: ignore
+from .icao_codes import law_enforcement_icao_set, military_icao_set
 
 class Skysearch(commands.Cog):
-    law_enforcement_icao_set = {'A10941', 'AB68C8', 'A9A449', 'A67117', 'A03325', 'A1B4BB', 'A2C934', 'AD933E', 'AC6E92', 'A64D15', 'A6A665', 'A97B90', 'A7C42B', 'A92BFF'}  # Update this set as needed or discovered
-    military_icao_set = {'AE055F', 'AE0270', 'AE1462', 'AE146E', 'AE0664', 'AE4E0D', 'AE0808', 'AE0800', 'AE146B'}
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=492089091320446976)  
