e5e39c27484276bfb9464f7d296c5dad45c46e57
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-11 01:42:55 -0600

    Update skysearch.py

--- skysearch/skysearch.py
+++ skysearch/skysearch.py
@@ -100,7 +100,7 @@ class Skysearch(commands.Cog):
                     emoji = ":arrow_upper_left:"
                 else:
                     emoji = ":arrow_up:"
-                embed.add_field(name="Heading", value=f"{emoji} `{heading} degrees`", inline=True)
+                embed.add_field(name="Heading", value=f"{emoji} `{heading}°`", inline=True)
             lat = aircraft_data.get('lat', 'N/A')
             lon = aircraft_data.get('lon', 'N/A')
             if lat != 'N/A':
