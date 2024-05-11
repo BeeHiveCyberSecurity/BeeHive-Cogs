a151e6fba6088538e7a3232c6f7200badd8f8011
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-10 20:13:17 -0600

    Update skysearch.py

--- skysearch/skysearch.py
+++ skysearch/skysearch.py
@@ -113,11 +113,11 @@ class Skysearch(commands.Cog):
             if baro_rate == 'N/A':
                 embed.add_field(name="Altitude trend", value=":grey_question: Altitude trends unavailable, not enough data...", inline=False)
             elif abs(int(baro_rate)) < 50:
-                embed.add_field(name="Altitude trend", value="<:pointright:1197006726466130072> **Maintaining altitude**\n" + f"`{baro_rate} feet/min`", inline=False)
+                embed.add_field(name="Altitude trend", value="<:pointright:1197006726466130072> **Maintaining altitude** @ " + f"`{baro_rate} feet/min`", inline=False)
             elif int(baro_rate) > 0:
-                embed.add_field(name="Altitude trend", value="<:pointup:1197006728953339924> **Climbing**\n" + f"`{baro_rate} feet/min`", inline=False)
+                embed.add_field(name="Altitude trend", value="<:pointup:1197006728953339924> **Climbing** @ " + f"`{baro_rate} feet/min`", inline=False)
             else:
-                embed.add_field(name="Altitude trend", value="<:pointdown:1197006724377366668> **Descending**\n" + f"`{abs(int(baro_rate))} feet/min`", inline=False)
+                embed.add_field(name="Altitude trend", value="<:pointdown:1197006724377366668> **Descending**@ " + f"`{abs(int(baro_rate))} feet/min`", inline=False)
             embed.add_field(name="Safety status", value=emergency_status, inline=True)
             view = discord.ui.View()
             view.add_item(discord.ui.Button(label=f"Track live", url=f"{link}", style=discord.ButtonStyle.link))
