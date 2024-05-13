1e430bcf2aa3343b2475813a94713e6e1b812224
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-13 08:49:09 -0600

    Tests

--- virustotal/virustotal.py
+++ virustotal/virustotal.py
@@ -1,14 +1,12 @@
 import aiohttp
-import asyncio
-import discord
-from redbot.core import commands
+import discord #type: ignore
+from redbot.core import commands #type: ignore
 
 class VirusTotal(commands.Cog):
     """VirusTotal file upload and analysis via Discord"""
 
     def __init__(self, bot):
         self.bot = bot
-        self.suspicious_file_types = ['.exe', '.dll', '.scr', '.zip', '.rar', '.bat', '.cmd', '.js', '.jar', '.docm', '.xlsm', '.pptm']
 
     @commands.bot_has_permissions(embed_links=True)
     @commands.hybrid_command(name="virustotal", description="Submit a file for analysis via VirusTotal", aliases=["vt"])
@@ -16,114 +14,113 @@ class VirusTotal(commands.Cog):
         async with ctx.typing():
             vt_key = await self.bot.get_shared_api_tokens("virustotal")
             if not vt_key.get("api_key"):
-                await ctx.send(embed=self.create_error_embed('No VirusTotal API Key set', "Your Red instance doesn't have an API key set for VirusTotal.\n\nUntil you add an API key using `[p]set api`, the VirusTotal API will refuse your requests and this cog won't work."))
+                embed = discord.Embed(title='Error: No VirusTotal API Key set', description="Your Red instance doesn't have an API key set for VirusTotal.\n\nUntil you add an API key using `[p]set api`, the VirusTotal API will refuse your requests and this cog won't work.", colour=discord.Colour.red())
+                embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
+                await ctx.send(embed=embed)
                 return
 
             async with aiohttp.ClientSession() as session:
                 try:
                     if file_url:
-                        await self.analyze_url(ctx, session, file_url, vt_key["api_key"])
+                        async with session.post("https://www.virustotal.com/api/v3/urls", headers={"x-apikey": vt_key["api_key"]}, data={"url": file_url}) as response:
+                            if response.status != 200:
+                                raise aiohttp.ClientResponseError(response.request_info, response.history, status=response.status, message=f"HTTP error {response.status}", headers=response.headers)
+                            data = await response.json()
+                            permalink = data.get("data", {}).get("id")
+                            if permalink:
+                                await ctx.send(f"Permalink: https://www.virustotal.com/gui/url/{permalink}")
+                                await self.check_results(ctx, permalink, ctx.author.id)
+                            else:
+                                raise ValueError("No permalink found in the response.")
                     elif ctx.message.attachments:
-                        await self.analyze_attachment(ctx, session, ctx.message.attachments[0], vt_key["api_key"])
+                        attachment = ctx.message.attachments[0]
+                        async with session.get(attachment.url) as response:
+                            if response.status != 200:
+                                raise aiohttp.ClientResponseError(response.request_info, response.history, status=response.status, message=f"HTTP error {response.status}", headers=response.headers)
+                            file_content = await response.read()
+                            async with session.post("https://www.virustotal.com/api/v3/files", headers={"x-apikey": vt_key["api_key"]}, data={"file": file_content}) as response:
+                                if response.status != 200:
+                                    raise aiohttp.ClientResponseError(response.request_info, response.history, status=response.status, message=f"HTTP error {response.status}", headers=response.headers)
+                                data = await response.json()
+                                analysis_id = data.get("data", {}).get("id")
+                                if analysis_id:
+                                    await self.check_results(ctx, analysis_id, ctx.author.id)
+                                else:
+                                    raise ValueError("No analysis ID found in the response.")
                     else:
-                        await ctx.send(embed=self.create_error_embed('No file provided', "The bot was unable to find content to submit for analysis!\nPlease provide one of the following when using this command:\n- URL file can be downloaded from\n- Drag-and-drop a file less than 25mb in size"))
+                        embed = discord.Embed(title='Error: No file provided', description="The bot was unable to find content to submit for analysis!\nPlease provide one of the following when using this command:\n- URL file can be downloaded from\n- Drag-and-drop a file less than 25mb in size", colour=discord.Colour.red())
+                        embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
+                        await ctx.send(embed=embed)
                 except (aiohttp.ClientResponseError, ValueError) as e:
-                    await ctx.send(embed=self.create_error_embed('Failed to submit file', str(e)))
+                    embed = discord.Embed(title='Error: Failed to submit file', description=str(e), colour=discord.Colour.red())
+                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
+                    await ctx.send(embed=embed)
                 except asyncio.TimeoutError:
-                    await ctx.send(embed=self.create_error_embed('Request Timeout', "The bot was unable to complete the request due to a timeout."))
-                except Exception as e:
-                    await ctx.send(embed=self.create_error_embed('Unexpected Error', f"An unexpected error occurred: {str(e)}"))
+                    embed = discord.Embed(title='Error: Request Timeout', description="The bot was unable to complete the request due to a timeout.", colour=discord.Colour.red())
+                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
+                    await ctx.send(embed=embed)
 
-    @commands.Cog.listener()
-    async def on_message_without_command(self, message):
-        if not message.attachments:
-            return
+    async def check_results(self, ctx, analysis_id, presid):
+        vt_key = await self.bot.get_shared_api_tokens("virustotal")
+        headers = {"x-apikey": vt_key["api_key"]}
 
-        for attachment in message.attachments:
-            if any(attachment.filename.lower().endswith(ext) for ext in self.suspicious_file_types):
-                async with aiohttp.ClientSession() as session:
-                    vt_key = await self.bot.get_shared_api_tokens("virustotal")
-                    if vt_key.get("api_key"):
-                        try:
-                            await self.analyze_attachment(message.channel, session, attachment, vt_key["api_key"])
-                        except Exception as e:
-                            await message.channel.send(embed=self.create_error_embed('Failed to analyze attachment', str(e)))
-
-    async def analyze_url(self, ctx, session, file_url, api_key):
-        try:
-            async with session.post("https://www.virustotal.com/api/v3/urls", headers={"x-apikey": api_key}, data={"url": file_url}) as response:
-                await self.handle_response(response, ctx, 'URL')
-        except aiohttp.ClientError as e:
-            await ctx.send(embed=self.create_error_embed('Client Error', f"An error occurred while analyzing the URL: {str(e)}"))
-
-    async def analyze_attachment(self, ctx, session, attachment, api_key):
-        try:
-            async with session.get(attachment.url) as response:
-                await self.handle_response(response, ctx, 'File')
-        except aiohttp.ClientError as e:
-            await ctx.send(embed=self.create_error_embed('Client Error', f"An error occurred while downloading the attachment: {str(e)}"))
-
-    async def handle_response(self, response, ctx, analysis_type):
-        if response.status != 200:
-            raise aiohttp.ClientResponseError(response.request_info, response.history, status=response.status, message=f"HTTP error {response.status}", headers=response.headers)
-        try:
-            data = await response.json()
-        except aiohttp.ContentTypeError:
-            raise ValueError(f"Invalid response received from {analysis_type} analysis: not a JSON response.")
-        except ValueError:
-            content = await response.text()
-            raise ValueError(f"Invalid response received from {analysis_type} analysis: not a JSON response. Response content: {content}")
-        analysis_id = data.get("data", {}).get("id")
-        if analysis_id:
-            await self.check_results(ctx, analysis_id, ctx.author.id)
-        else:
-            raise ValueError(f"No analysis ID found in the {analysis_type} response.")
-
-    async def check_results(self, ctx, analysis_id, user_id):
-        # Check the analysis results periodically until the analysis is complete
         async with aiohttp.ClientSession() as session:
-            headers = {"x-apikey": await self.bot.get_shared_api_tokens("virustotal").get("api_key")}
-            url = f"https://www.virustotal.com/api/v3/analyses/{analysis_id}"
             while True:
                 try:
-                    async with session.get(url, headers=headers) as response:
+                    async with session.get(f'https://www.virustotal.com/api/v3/analyses/{analysis_id}', headers=headers) as response:
                         if response.status != 200:
                             raise aiohttp.ClientResponseError(response.request_info, response.history, status=response.status, message=f"HTTP error {response.status}", headers=response.headers)
-                        result_data = await response.json()
-                        status = result_data.get("data", {}).get("attributes", {}).get("status")
-                        if status == "completed":
-                            break
-                        await asyncio.sleep(5)  # Wait for 5 seconds before checking again
-                except aiohttp.ClientError as e:
-                    await ctx.send(embed=self.create_error_embed('Client Error', f"An error occurred while checking the analysis results: {str(e)}"))
-                    return
-
-            # Once the analysis is complete, send the results to the user
-            analysis_results = result_data.get("data", {}).get("attributes", {}).get("results", {})
-            embed = self.create_results_embed(analysis_results)
-            user = self.bot.get_user(user_id)
-            if user:
-                try:
-                    await user.send(embed=embed)
-                except discord.HTTPException as e:
-                    await ctx.send(embed=self.create_error_embed('Send Error', f"Failed to send results to the user: {str(e)}"))
-            else:
-                await ctx.send(embed=self.create_error_embed('User not found', "The bot was unable to send the results to the user."))
-
-    def create_results_embed(self, results):
-        # Create an embed with the analysis results
-        embed = discord.Embed(title='VirusTotal Analysis Results', colour=discord.Colour.green())
-        embed.add_field(name='Harmless', value=results.get('harmless', 0), inline=True)
-        embed.add_field(name='Malicious', value=results.get('malicious', 0), inline=True)
-        embed.add_field(name='Suspicious', value=results.get('suspicious', 0), inline=True)
-        embed.add_field(name='Undetected', value=results.get('undetected', 0), inline=True)
-        embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/check-circle-outline.png")
-        return embed
-
-    def create_error_embed(self, title, description):
-        embed = discord.Embed(title=f'Error: {title}', description=description, colour=discord.Colour.red())
-        embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
-        return embed
+                        data = await response.json()
+                        attributes = data.get("data", {}).get("attributes", {})
+                        if attributes.get("status") == "completed":
+                            stats = attributes.get("stats", {})
+                            malicious_count = stats.get("malicious", 0)
+                            suspicious_count = stats.get("suspicious", 0)
+                            undetected_count = stats.get("undetected", 0)
+                            harmless_count = stats.get("harmless", 0)
+                            failure_count = stats.get("failure", 0)
+                            unsupported_count = stats.get("type-unsupported", 0)
+                            meta = data.get("meta", {}).get("file_info", {}).get("sha256")
+                            if meta:
+                                embed = discord.Embed(url=f"https://www.virustotal.com/gui/file/{meta}")
+                                content = f"||<@{presid}>||"
+                                if malicious_count >= 11:
+                                    embed.title = "That file looks malicious!"
+                                    embed.description = "You should avoid running, using, or handling the file out of an abundance of caution."
+                                    embed.color = discord.Colour.red()
+                                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/warning-outline.png")
+                                elif 1 < malicious_count < 11:
+                                    embed.title = "That file looks suspicious!"
+                                    embed.description = "This file might be harmful. Exercise caution and consider further analysis."
+                                    embed.color = discord.Colour.orange()
+                                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Orange/alert-outline.png")
+                                else:
+                                    embed.title = "That file looks safe!"
+                                    embed.color = discord.Colour.green()
+                                    embed.description = "You should be safe to use this file.\nWant a [second opinion?](https://discord.gg/6PbaH6AfvF)"
+                                    embed.add_field(name="Overall verdict", value="Clean", inline=False)
+                                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Green/checkmark-circle-outline.png")
+                                total_count = malicious_count + suspicious_count + undetected_count + harmless_count + failure_count + unsupported_count
+                                noanswer_count = failure_count + unsupported_count
+                                safe_count = harmless_count + undetected_count
+                                percent = round((malicious_count / total_count) * 100, 2) if total_count > 0 else 0
+                                embed.add_field(name="Analysis results", value=f"**{percent}%** of security vendors rated this file dangerous!\n- **{malicious_count}** malicious\n- **{suspicious_count}** suspicious\n- **{safe_count}** detected no threats\n- **{noanswer_count}** engines couldn't check this file.", inline=False)
+                                await ctx.send(content, embed=embed)
+                                break
+                            else:
+                                raise ValueError("SHA256 value not found in the analysis response.")
+                        else:
+                            await asyncio.sleep(3)
+                except (aiohttp.ClientResponseError, ValueError) as e:
+                    embed = discord.Embed(title='Error: Analysis Failed', description=str(e), colour=discord.Colour.red())
+                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
+                    await ctx.send(embed=embed)
+                    break
+                except asyncio.TimeoutError:
+                    embed = discord.Embed(title='Error: Request Timeout', description="The bot was unable to complete the request due to a timeout.", colour=discord.Colour.red())
+                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
+                    await ctx.send(embed=embed)
+                    break
 
 
 
