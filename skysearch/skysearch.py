02221aa68c34d39209cb6fc644ef2b2f0f164b58
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-12 23:22:23 -0600

    Formatting

--- skysearch/skysearch.py
+++ skysearch/skysearch.py
@@ -1041,7 +1041,7 @@ class Skysearch(commands.Cog):
                             embed.add_field(name="Runway status", value=runway_status, inline=True)
 
                             if  'le_ident' in runway:
-                                embed.add_field(name="Low End", value=f"`{runway[le_ident]}", inline=False)
+                                embed.add_field(name="Low End", value=f"`{runway['le_ident']}", inline=False)
                             
                             runway_pages.append(embed)
 
