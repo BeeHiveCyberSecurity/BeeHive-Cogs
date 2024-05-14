e694a1f44310318feec6b1afb299315b1f206d8a
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-14 00:57:32 -0600

    Mistakes were fucking made

--- virustotal/virustotal.py
+++ virustotal/virustotal.py
@@ -76,97 +76,98 @@ class VirusTotal(commands.Cog):
         headers = {"x-apikey": vt_key["api_key"]}
 
         async with aiohttp.ClientSession() as session:
-            while True:
-                try:
-                    async with session.get(f'https://www.virustotal.com/api/v3/analyses/{analysis_id}', headers=headers) as response:
-                        if response.status != 200:
-                            raise aiohttp.ClientResponseError(response.request_info, response.history, status=response.status, message=f"HTTP error {response.status}", headers=response.headers)
-                        data = await response.json()
-                        attributes = data.get("data", {}).get("attributes", {})
-                        if attributes.get("status") == "completed":
-                            stats = attributes.get("stats", {})
-                            malicious_count = stats.get("malicious", 0)
-                            suspicious_count = stats.get("suspicious", 0)
-                            undetected_count = stats.get("undetected", 0)
-                            harmless_count = stats.get("harmless", 0)
-                            failure_count = stats.get("failure", 0)
-                            unsupported_count = stats.get("type-unsupported", 0)
-                            meta = data.get("meta", {}).get("file_info", {})
-                            sha256 = meta.get("sha256")
-                            sha1 = meta.get("sha1")
-                            md5 = meta.get("md5")
+            try:
+                async with session.get(f'https://www.virustotal.com/api/v3/analyses/{analysis_id}', headers=headers) as response:
+                    if response.status != 200:
+                        raise aiohttp.ClientResponseError(response.request_info, response.history, status=response.status, message=f"HTTP error {response.status}", headers=response.headers)
+                    data = await response.json()
+                    attributes = data.get("data", {}).get("attributes", {})
+                    while attributes.get("status") != "completed":
+                        await asyncio.sleep(3)
+                        async with session.get(f'https://www.virustotal.com/api/v3/analyses/{analysis_id}', headers=headers) as response:
+                            if response.status != 200:
+                                raise aiohttp.ClientResponseError(response.request_info, response.history, status=response.status, message=f"HTTP error {response.status}", headers=response.headers)
+                            data = await response.json()
+                            attributes = data.get("data", {}).get("attributes", {})
+                    
+                    stats = attributes.get("stats", {})
+                    malicious_count = stats.get("malicious", 0)
+                    suspicious_count = stats.get("suspicious", 0)
+                    undetected_count = stats.get("undetected", 0)
+                    harmless_count = stats.get("harmless", 0)
+                    failure_count = stats.get("failure", 0)
+                    unsupported_count = stats.get("type-unsupported", 0)
+                    meta = data.get("meta", {}).get("file_info", {})
+                    sha256 = meta.get("sha256")
+                    sha1 = meta.get("sha1")
+                    md5 = meta.get("md5")
 
-                            total_count = malicious_count + suspicious_count + undetected_count + harmless_count + failure_count + unsupported_count
-                            noanswer_count = failure_count + unsupported_count
-                            safe_count = harmless_count + undetected_count
-                            percent = round((malicious_count / total_count) * 100, 2) if total_count > 0 else 0
-                            if sha256 and sha1 and md5:
-                                embed = discord.Embed()
-                                content = f"||<@{presid}>||"
-                                labels = 'Malicious', 'Suspicious', 'Undetected', 'Harmless', 'Failure', 'Unsupported'
-                                sizes = [malicious_count, suspicious_count, undetected_count, harmless_count, failure_count, unsupported_count]
-                                colors = ['#ff4545', '#ff9144', '#dddddd', '#2BBD8E', '#ffcccb', '#ececec']
-                                explode = (0.1 if malicious_count > 0 else 0, 0, 0, 0, 0, 0)  # explode the first slice if there are malicious results
+                    total_count = malicious_count + suspicious_count + undetected_count + harmless_count + failure_count + unsupported_count
+                    noanswer_count = failure_count + unsupported_count
+                    safe_count = harmless_count + undetected_count
+                    percent = round((malicious_count / total_count) * 100, 2) if total_count > 0 else 0
+                    if sha256 and sha1 and md5:
+                        embed = discord.Embed()
+                        content = f"||<@{presid}>||"
+                        labels = 'Malicious', 'Suspicious', 'Undetected', 'Harmless', 'Failure', 'Unsupported'
+                        sizes = [malicious_count, suspicious_count, undetected_count, harmless_count, failure_count, unsupported_count]
+                        colors = ['#ff4545', '#ff9144', '#dddddd', '#2BBD8E', '#ffcccb', '#ececec']
+                        explode = (0.1 if malicious_count > 0 else 0, 0, 0, 0, 0, 0)  # explode the first slice if there are malicious results
 
-                                # Plot
-                                plt.figure(figsize=(6, 4))
-                                plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
-                                plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+                        # Plot
+                        plt.figure(figsize=(6, 4))
+                        plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+                        plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-                                # Save the pie chart as a PNG image in memory.
-                                pie_chart_buffer = io.BytesIO()
-                                plt.savefig(pie_chart_buffer, format='png')
-                                pie_chart_buffer.seek(0)  # rewind the buffer to the beginning so we can read its content
+                        # Save the pie chart as a PNG image in memory.
+                        pie_chart_buffer = io.BytesIO()
+                        plt.savefig(pie_chart_buffer, format='png')
+                        pie_chart_buffer.seek(0)  # rewind the buffer to the beginning so we can read its content
 
-                                # Set the pie chart image as the embed image
-                                embed.set_image(url="attachment://pie_chart.png")
+                        # Set the pie chart image as the embed image
+                        embed.set_image(url="attachment://pie_chart.png")
 
-                                # Clear the matplotlib figure
-                                plt.clf()
-                                plt.close('all')
+                        # Clear the matplotlib figure
+                        plt.clf()
+                        plt.close('all')
 
-                                # Create a discord file from the image in memory
-                                pie_chart_file = discord.File(pie_chart_buffer, filename='pie_chart.png')
+                        # Create a discord file from the image in memory
+                        pie_chart_file = discord.File(pie_chart_buffer, filename='pie_chart.png')
 
-                                if malicious_count >= 11:
-                                    embed.title = "Malicious file found"
-                                    embed.description = f"### {int(percent)}% of security vendors rated this file dangerous!\n- **{malicious_count}** malicious\n- **{suspicious_count}** suspicious\n- **{safe_count}** detected no threats\n- **{noanswer_count}** engines couldn't check this file."
-                                    embed.color = discord.Colour(0xff4545)
-                                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/warning-outline.png")
-                                elif 1 < malicious_count < 11:
-                                    embed.title = "Suspicious file found"
-                                    embed.description = f"### {int(percent)}% of security vendors rated this file dangerous!\n- **{malicious_count}** malicious\n- **{suspicious_count}** suspicious\n- **{safe_count}** detected no threats\n- **{noanswer_count}** engines couldn't check this file."
-                                    embed.color = discord.Colour(0xff9144)
-                                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Orange/alert-outline.png")
-                                else:
-                                    embed.title = "No threat found"
-                                    embed.color = discord.Colour(0x2BBD8E)
-                                    embed.description = "### No security vendors currently flag this file as malicious - it should be safe to use."
-                                    embed.add_field(name="Overall verdict", value="Clean", inline=False)
-                                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Green/checkmark-circle-outline.png")
-
-                                embed.add_field(name="SHA-256", value=f"`{sha256}`", inline=False)
-                                embed.add_field(name="SHA-1", value=f"`{sha1}`", inline=False)
-                                embed.add_field(name="MD5", value=f"`{md5}`", inline=False)
-                                # Create the button for the virustotal results link
-                                button = discord.ui.Button(label="View results on VirusTotal", url=f"https://www.virustotal.com/gui/file/{sha256}", emoji="🌐", style=discord.ButtonStyle.url)
-                                button2 = discord.ui.Button(label="Get a second opinion", url="https://discord.gg/6PbaH6AfvF", emoji="", style=discord.ButtonStyle.url)
-                                view = discord.ui.View()
-                                view.add_item(button)
-                                await ctx.send(content=content, file=pie_chart_file, embed=embed, view=view)
-                            else:
-                                raise ValueError("Required hash values not found in the analysis response.")
-                                break
+                        if malicious_count >= 11:
+                            embed.title = "Malicious file found"
+                            embed.description = f"### {int(percent)}% of security vendors rated this file dangerous!\n- **{malicious_count}** malicious\n- **{suspicious_count}** suspicious\n- **{safe_count}** detected no threats\n- **{noanswer_count}** engines couldn't check this file."
+                            embed.color = discord.Colour(0xff4545)
+                            embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/warning-outline.png")
+                        elif 1 < malicious_count < 11:
+                            embed.title = "Suspicious file found"
+                            embed.description = f"### {int(percent)}% of security vendors rated this file dangerous!\n- **{malicious_count}** malicious\n- **{suspicious_count}** suspicious\n- **{safe_count}** detected no threats\n- **{noanswer_count}** engines couldn't check this file."
+                            embed.color = discord.Colour(0xff9144)
+                            embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Orange/alert-outline.png")
                         else:
-                            await asyncio.sleep(3)
-                except (aiohttp.ClientResponseError, ValueError) as e:
-                    embed = discord.Embed(title='Error: Analysis Failed', description=str(e), colour=discord.Colour.red())
-                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
-                    await ctx.send(embed=embed)
-                    break
-                except asyncio.TimeoutError:
-                    embed = discord.Embed(title='Error: Request Timeout', description="The bot was unable to complete the request due to a timeout.", colour=discord.Colour.red())
-                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
-                    await ctx.send(embed=embed)
-                    break
+                            embed.title = "No threat found"
+                            embed.color = discord.Colour(0x2BBD8E)
+                            embed.description = "### No security vendors currently flag this file as malicious - it should be safe to use."
+                            embed.add_field(name="Overall verdict", value="Clean", inline=False)
+                            embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Green/checkmark-circle-outline.png")
+
+                        embed.add_field(name="SHA-256", value=f"`{sha256}`", inline=False)
+                        embed.add_field(name="SHA-1", value=f"`{sha1}`", inline=False)
+                        embed.add_field(name="MD5", value=f"`{md5}`", inline=False)
+                        # Create the button for the virustotal results link
+                        button = discord.ui.Button(label="View results on VirusTotal", url=f"https://www.virustotal.com/gui/file/{sha256}", emoji="🌐", style=discord.ButtonStyle.url)
+                        button2 = discord.ui.Button(label="Get a second opinion", url="https://discord.gg/6PbaH6AfvF", emoji="", style=discord.ButtonStyle.url)
+                        view = discord.ui.View()
+                        view.add_item(button)
+                        await ctx.send(content=content, file=pie_chart_file, embed=embed, view=view)
+                    else:
+                        raise ValueError("Required hash values not found in the analysis response.")
+            except (aiohttp.ClientResponseError, ValueError) as e:
+                embed = discord.Embed(title='Error: Analysis Failed', description=str(e), colour=discord.Colour.red())
+                embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
+                await ctx.send(embed=embed)
+            except asyncio.TimeoutError:
+                embed = discord.Embed(title='Error: Request Timeout', description="The bot was unable to complete the request due to a timeout.", colour=discord.Colour.red())
+                embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
+                await ctx.send(embed=embed)
 
