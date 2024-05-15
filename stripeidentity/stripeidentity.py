7cf57ea4a6faaa32cdef8d745efe57853374e211
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-14 19:56:37 -0600

    Formatting

--- stripeidentity/stripeidentity.py
+++ stripeidentity/stripeidentity.py
@@ -261,9 +261,9 @@ class StripeIdentity(commands.Cog):
                 description=(
                     f"Hello {user.mention},\n\n"
                     f"To enhance safety and security within our community, **{ctx.guild.name}** requires you to verify your identity, and link it to your Discord account. \n\n"
-                    "> This procedure involves the confirmation of your identity using a government-issued ID and biometric verification.\n\n"
-                    "### Please ensure you have one of the following documents:\n- **State ID**\n- **Driver's License**\n- **Driver's Permit**\n- **Passport**\n\n"
-                    "### You will also need to:\n- Provide a valid email address\n- Take a series of selfies in a well-lit area\n- Submit the identity document mentioned above for biometric matching and analysis\n\n"
+                    "> This procedure involves the confirmation of your identity using a government-issued ID and biometric verification.\n"
+                    "### Please ensure you have one of the following documents:\n- **State ID**\n- **Driver's License**\n- **Driver's Permit**\n- **Passport**\n"
+                    "### You will also need to:\n- **Provide a valid email address**\n- **Take a series of selfies in a well-lit area**\n- **Submit the identity document mentioned above for biometric matching and analysis**\n\n"
                     "Upon completing the verification, your personal information will be handled according to the following legal agreements:\n- **[BeeHive Terms of Service](<https://www.beehive.systems/tos)**\n- **[BeeHive Privacy Policy](https://www.beehive.systems/privacy)**\n- **[Stripe Privacy Policy](https://stripe.com/privacy)**\n- **[Stripe Consumer Terms of Service](https://stripe.com/legal/consumer)**\n\n"
                     "Should you choose not to provide your personal information, you can opt out of the verification process by selecting the option below. This action will result in your immediate removal from the server. If no action is taken within **15 minutes**, you will be automatically removed from the server."
                 ),
