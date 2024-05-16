aa7387dfae8b40a4394993012c1ef0df83a307d2
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-15 18:31:13 -0600

    Increase zoom

--- skysearch/skysearch.py
+++ skysearch/skysearch.py
@@ -1125,7 +1125,7 @@ class Skysearch(commands.Cog):
                 street_view_base_url = "https://maps.googleapis.com/maps/api/staticmap"
                 street_view_params = {
                     "size": "600x400",
-                    "zoom": "13",
+                    "zoom": "14",
                     "scale": "2",  # Image size (width x height)
                     "center": f"{data1['latitude']},{data1['longitude']}",  # Latitude and Longitude as comma-separated string
                     "maptype": "hybrid",
