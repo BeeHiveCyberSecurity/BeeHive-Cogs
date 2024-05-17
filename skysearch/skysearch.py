00f62df8513be73f00b0157962dc104fc8b4c3c8
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-16 19:36:14 -0600

    Tests

--- skysearch/skysearch.py
+++ skysearch/skysearch.py
@@ -186,12 +186,12 @@ class Skysearch(commands.Cog):
             
             last_seen = aircraft_data.get('seen', 'N/A')
             if last_seen != 'N/A':
-                last_seen_text = "Just now" if float(last_seen) < 1 else f"**`{last_seen}`** seconds ago"
+                last_seen_text = ":green_circle: **`Just now`**" if float(last_seen) < 1 else f"**:hourglass: `{last_seen} seconds ago`**"
                 embed.add_field(name="Last check in", value=last_seen_text, inline=False)
             
             last_seen_pos = aircraft_data.get('seen_pos', 'N/A')
             if last_seen_pos != 'N/A':
-                last_seen_pos_text = "Just now" if float(last_seen_pos) < 1 else f"**`{last_seen_pos}`** seconds ago"
+                last_seen_pos_text = ":green_circle: **`Just now`**" if float(last_seen_pos) < 1 else f":hourglass **`{last_seen_pos} seconds ago`**"
                 embed.add_field(name="Last position update", value=last_seen_pos_text, inline=False)
             
             baro_rate = aircraft_data.get('baro_rate', 'N/A')
