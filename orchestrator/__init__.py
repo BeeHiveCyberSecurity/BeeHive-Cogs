d71984e025eefe3a5de330cff60c071ed7b4c81a
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-17 06:10:33 -0600

    This cog was a waste anyway

--- orchestrator/__init__.py
+++
@@ -1,7 +0,0 @@
-from .orchestrator import Orchestrator
-from redbot.core.utils import get_end_user_data_statement
-
-__red_end_user_data_statement__ = "This cog does not store any End User Data."
-
-async def setup(bot):
-    await bot.add_cog(Orchestrator(bot))
