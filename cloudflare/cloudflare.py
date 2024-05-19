7eea9addfd7cfbf28edb636cfb62af374d11f564
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-19 12:16:48 -0600

    Formatting

--- cloudflare/cloudflare.py
+++ cloudflare/cloudflare.py
@@ -995,8 +995,8 @@ class Cloudflare(commands.Cog):
             embed.add_field(name="ID", value=f"**`{settings.get('id', 'N/A').upper()}`**", inline=False)
             embed.add_field(name="Modified", value=f"**`{settings.get('modified', 'N/A')}`**", inline=False)
             embed.add_field(name="Name", value=f"**`{settings.get('name', 'N/A')}`**", inline=False)
-            embed.add_field(name="Skipped wizard", value=f"**`{settings.get('skip_wizard', 'N/A')}`**", inline=False)
-            embed.add_field(name="Status", value=f"**`{settings.get('status', 'N/A')}`**", inline=False)
+            embed.add_field(name="Skipped wizard", value=f"**`{settings.get('skip_wizard', 'N/A').upper()}`**", inline=False)
+            embed.add_field(name="Status", value=f"**`{settings.get('status', 'N/A').upper()}`**", inline=False)
             embed.add_field(name="Synced", value=f"**`{settings.get('synced', 'N/A').upper()}`**", inline=False)
             embed.add_field(name="Tag", value=f"**`{settings.get('tag', 'N/A').upper()}`**", inline=False)
 
