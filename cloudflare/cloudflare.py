478d98a422dd4078a5748da2d4d8c526716f1efc
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-19 22:21:10 -0600

    Shorten

--- cloudflare/cloudflare.py
+++ cloudflare/cloudflare.py
@@ -714,7 +714,7 @@ class Cloudflare(commands.Cog):
     @commands.is_owner()
     @commands.group()
     async def emailrouting(self, ctx):
-        """Cloudflare Email Routing is designed to simplify the way you create and manage email addresses, without needing to keep an eye on additional mailboxes. With Email Routing, you can create any number of custom email addresses to use in situations where you do not want to share your primary email address, such as when you subscribe to a new service or newsletter. Emails are then routed to your preferred email inbox, without you ever having to expose your primary email address. Email Routing is free and private by design. Cloudflare will not store or access the emails routed to your inbox.It is available to all Cloudflare customers using Cloudflare as an authoritative nameserver."""
+        """Cloudflare Email Routing is designed to simplify the way you create and manage email addresses, without needing to keep an eye on additional mailboxes. Learn more at https://developers.cloudflare.com/email-routing/"""
         if ctx.invoked_subcommand is None:
             await ctx.send("Invalid Cloudflare command passed.")
 
