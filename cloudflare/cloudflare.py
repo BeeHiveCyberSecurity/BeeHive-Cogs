86c6f595ac0bdfb83a5c6b276014212f53f86f9f
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-19 22:18:53 -0600

    Tests

--- cloudflare/cloudflare.py
+++ cloudflare/cloudflare.py
@@ -714,7 +714,7 @@ class Cloudflare(commands.Cog):
     @commands.is_owner()
     @commands.group()
     async def emailrouting(self, ctx):
-        """Manage Cloudflare Email Routing"""
+        """Cloudflare Email Routing is designed to simplify the way you create and manage email addresses, without needing to keep an eye on additional mailboxes. With Email Routing, you can create any number of custom email addresses to use in situations where you do not want to share your primary email address, such as when you subscribe to a new service or newsletter. Emails are then routed to your preferred email inbox, without you ever having to expose your primary email address. Email Routing is free and private by design. Cloudflare will not store or access the emails routed to your inbox.It is available to all Cloudflare customers using Cloudflare as an authoritative nameserver."""
         if ctx.invoked_subcommand is None:
             await ctx.send("Invalid Cloudflare command passed.")
 
@@ -1162,7 +1162,7 @@ class Cloudflare(commands.Cog):
     @commands.is_owner()
     @emailrouting.group(name="rules", invoke_without_command=True)
     async def email_routing_rules(self, ctx):
-        """Cloudflare Email Routing is designed to simplify the way you create and manage email addresses, without needing to keep an eye on additional mailboxes. With Email Routing, you can create any number of custom email addresses to use in situations where you do not want to share your primary email address, such as when you subscribe to a new service or newsletter. Emails are then routed to your preferred email inbox, without you ever having to expose your primary email address. Email Routing is free and private by design. Cloudflare will not store or access the emails routed to your inbox.It is available to all Cloudflare customers using Cloudflare as an authoritative nameserver."""
+        """Manage your Email Routing rules"""
         await ctx.send_help(ctx.command)
 
     @commands.is_owner()
