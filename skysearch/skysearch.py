07b84ec6db29d15975d08c4a92402bbad7f10080
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-05-22 03:58:26 -0600

    Shorten labels

--- skysearch/skysearch.py
+++ skysearch/skysearch.py
@@ -182,12 +182,12 @@ class Skysearch(commands.Cog):
             last_seen = aircraft_data.get('seen', 'N/A')
             if last_seen != 'N/A':
                 last_seen_text = ":green_circle: **Just now**" if float(last_seen) < 1 else f":hourglass: **{int(float(last_seen))} seconds ago**"
-                embed.add_field(name="Last check in", value=last_seen_text, inline=True)
+                embed.add_field(name="Last signal", value=last_seen_text, inline=True)
             
             last_seen_pos = aircraft_data.get('seen_pos', 'N/A')
             if last_seen_pos != 'N/A':
                 last_seen_pos_text = ":green_circle: **Just now**" if float(last_seen_pos) < 1 else f":hourglass: **{int(float(last_seen_pos))} seconds ago**"
-                embed.add_field(name="Last position update", value=last_seen_pos_text, inline=True)
+                embed.add_field(name="Last position", value=last_seen_pos_text, inline=True)
             
             baro_rate = aircraft_data.get('baro_rate', 'N/A')
             if baro_rate == 'N/A':
