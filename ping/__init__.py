06a65e2c5d6e07c9030e13fa879d5f86926dff2d
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-07-28 19:53:36 -0600

    Tweaks

--- ping/__init__.py
+++ ping/__init__.py
@@ -1,4 +1,9 @@
 from .ping import Ping
+from redbot.core import commands
+
+class Ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
 async def setup(bot):
     await bot.add_cog(Ping(bot))
