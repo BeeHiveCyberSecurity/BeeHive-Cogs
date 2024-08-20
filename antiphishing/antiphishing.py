949173d7b33452685d290cac5b7b148cee3e16f2
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-08-20 07:23:41 -0600

    Versioning

--- antiphishing/antiphishing.py
+++ antiphishing/antiphishing.py
@@ -20,7 +20,7 @@ class AntiPhishing(commands.Cog):
     Guard users from malicious links and phishing attempts with customizable protection options.
     """
 
-    __version__ = "1.3.4.2"
+    __version__ = "1.3.4.4"
     __last_updated__ = "Aug 20 2024"
 
     def __init__(self, bot: Red):
