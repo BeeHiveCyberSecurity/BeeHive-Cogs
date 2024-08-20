e19c4e8739678d1a4207d34f2c53eb85d95fe8a6
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-08-20 07:32:20 -0600

    Updates

--- antiphishing/antiphishing.py
+++ antiphishing/antiphishing.py
@@ -20,7 +20,7 @@ class AntiPhishing(commands.Cog):
     Guard users from malicious links and phishing attempts with customizable protection options.
     """
 
-    __version__ = "1.3.5.0"
+    __version__ = "1.3.5.1"
     __last_updated__ = "Aug 20 2024"
 
     def __init__(self, bot: Red):
@@ -359,13 +359,13 @@ class AntiPhishing(commands.Cog):
             try:
                 embed = discord.Embed(
                     title="Link safety",
-                    description="No known threats were detected attached to this link. If in doubt, investigate further. Stay safe!",
+                    description=f"No known threats were detected attached to this link: {message.content}. If in doubt, investigate further. Stay safe!",
                     colour=0x2bbd8e,
                 )
                 embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Green/shield-checkmark.png")
                 await user.send(embed=embed)
             except discord.Forbidden:
-                await reaction.message.channel.send(f"{user.mention}, I couldn't send you a DM, but no known threats were detected attached to this link. If in doubt, investigate further. Stay safe!")
+                await reaction.message.channel.send(f"{user.mention}, I couldn't send you a DM, but no known threats were detected attached to this link: {message.content}. If in doubt, investigate further. Stay safe!")
 
     @commands.Cog.listener()
     async def on_member_update(self, before: discord.Member, after: discord.Member):
