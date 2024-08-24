8d33e3ed97412bed34cde0bab81fce65c170cf7f
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-08-23 19:43:02 -0600

    Adding plaintext

--- virustotal/virustotal.py
+++ virustotal/virustotal.py
@@ -24,9 +24,12 @@ class VirusTotal(commands.Cog):
         async with ctx.typing():
             vt_key = await self.bot.get_shared_api_tokens("virustotal")
             if not vt_key.get("api_key"):
-                embed = discord.Embed(title='Error: No VirusTotal API Key set', description="Your Red instance doesn't have an API key set for VirusTotal.\n\nUntil you add an API key using `[p]set api`, the VirusTotal API will refuse your requests and this cog won't work.", colour=discord.Colour(0xff4545))
-                embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/cog.png")
-                await ctx.send(embed=embed)
+                if ctx.channel.permissions_for(ctx.guild.me).embed_links:
+                    embed = discord.Embed(title='Error: No VirusTotal API Key set', description="Your Red instance doesn't have an API key set for VirusTotal.\n\nUntil you add an API key using `[p]set api`, the VirusTotal API will refuse your requests and this cog won't work.", colour=discord.Colour(0xff4545))
+                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/cog.png")
+                    await ctx.send(embed=embed)
+                else:
+                    await ctx.send("Error: No VirusTotal API Key set. Your Red instance doesn't have an API key set for VirusTotal. Until you add an API key using `[p]set api`, the VirusTotal API will refuse your requests and this cog won't work.")
                 return
 
             async with aiohttp.ClientSession() as session:
@@ -51,16 +54,22 @@ class VirusTotal(commands.Cog):
                     elif attachments:
                         attachment = attachments[0]
                         if attachment.size > 30 * 1024 * 1024:  # 30 MB limit
-                            embed = discord.Embed(title='Error: File too large', description="The file you provided exceeds the 30MB size limit for analysis.", colour=discord.Colour(0xff4545))
-                            await ctx.send(embed=embed)
+                            if ctx.channel.permissions_for(ctx.guild.me).embed_links:
+                                embed = discord.Embed(title='Error: File too large', description="The file you provided exceeds the 30MB size limit for analysis.", colour=discord.Colour(0xff4545))
+                                await ctx.send(embed=embed)
+                            else:
+                                await ctx.send("Error: File too large. The file you provided exceeds the 30MB size limit for analysis.")
                             return
                         async with session.get(attachment.url) as response:
                             if response.status != 200:
                                 raise aiohttp.ClientResponseError(response.request_info, response.history, status=response.status, message=f"HTTP error {response.status}", headers=response.headers)
                             file_content = await response.read()
                             file_name = attachment.filename  # Get the file name from the attachment
-                            embed = discord.Embed(title="Starting analysis", description="This could take a few minutes, please be patient. You'll be mentioned when results are available.", colour=discord.Colour(0x2BBD8E))
-                            await ctx.send(embed=embed)
+                            if ctx.channel.permissions_for(ctx.guild.me).embed_links:
+                                embed = discord.Embed(title="Starting analysis", description="This could take a few minutes, please be patient. You'll be mentioned when results are available.", colour=discord.Colour(0x2BBD8E))
+                                await ctx.send(embed=embed)
+                            else:
+                                await ctx.send("Starting analysis. This could take a few minutes, please be patient. You'll be mentioned when results are available.")
                             async with session.post("https://www.virustotal.com/api/v3/files", headers={"x-apikey": vt_key["api_key"]}, data={"file": file_content}) as response:
                                 if response.status != 200:
                                     raise aiohttp.ClientResponseError(response.request_info, response.history, status=response.status, message=f"HTTP error {response.status}", headers=response.headers)
@@ -73,17 +82,26 @@ class VirusTotal(commands.Cog):
                                 else:
                                     raise ValueError("No analysis ID found in the response.")
                     else:
-                        embed = discord.Embed(title='No file provided', description="The bot was unable to find content to submit for analysis!\nPlease provide one of the following when using this command:\n- URL file can be downloaded from\n- Drag-and-drop a file less than 30mb in size\n- Reply to a message containing a file", colour=discord.Colour(0xff4545))
+                        if ctx.channel.permissions_for(ctx.guild.me).embed_links:
+                            embed = discord.Embed(title='No file provided', description="The bot was unable to find content to submit for analysis!\nPlease provide one of the following when using this command:\n- URL file can be downloaded from\n- Drag-and-drop a file less than 30mb in size\n- Reply to a message containing a file", colour=discord.Colour(0xff4545))
+                            embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close.png")
+                            await ctx.send(embed=embed)
+                        else:
+                            await ctx.send("No file provided. The bot was unable to find content to submit for analysis! Please provide one of the following when using this command:\n- URL file can be downloaded from\n- Drag-and-drop a file less than 30mb in size\n- Reply to a message containing a file")
+                except (aiohttp.ClientResponseError, ValueError) as e:
+                    if ctx.channel.permissions_for(ctx.guild.me).embed_links:
+                        embed = discord.Embed(title='Failed to submit file', description=str(e), colour=discord.Colour(0xff4545))
                         embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close.png")
                         await ctx.send(embed=embed)
-                except (aiohttp.ClientResponseError, ValueError) as e:
-                    embed = discord.Embed(title='Failed to submit file', description=str(e), colour=discord.Colour(0xff4545))
-                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close.png")
-                    await ctx.send(embed=embed)
+                    else:
+                        await ctx.send(f"Failed to submit file: {str(e)}")
                 except asyncio.TimeoutError:
-                    embed = discord.Embed(title='Request timed out', description="The bot was unable to complete the request due to a timeout.", colour=discord.Colour(0xff4545))
-                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close.png")
-                    await ctx.send(embed=embed)
+                    if ctx.channel.permissions_for(ctx.guild.me).embed_links:
+                        embed = discord.Embed(title='Request timed out', description="The bot was unable to complete the request due to a timeout.", colour=discord.Colour(0xff4545))
+                        embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close.png")
+                        await ctx.send(embed=embed)
+                    else:
+                        await ctx.send("Request timed out. The bot was unable to complete the request due to a timeout.")
 
     async def check_results(self, ctx, analysis_id, presid, file_url, file_name):
         vt_key = await self.bot.get_shared_api_tokens("virustotal")
@@ -121,40 +139,55 @@ class VirusTotal(commands.Cog):
                     safe_count = harmless_count + undetected_count
                     percent = round((malicious_count / total_count) * 100, 2) if total_count > 0 else 0
                     if sha256 and sha1 and md5:
-                        embed = discord.Embed()
-                        content = f"||<@{presid}>||"
-                        if malicious_count >= 11:
-                            embed.title = "Analysis complete"
-                            embed.description = f"**{int(percent)}%** of vendors rated this file dangerous! You should avoid this file completely, and delete it from your systems to ensure security."
-                            embed.color = discord.Colour(0xff4545)
-                            embed.set_footer(text=f"SHA1 | {sha1}")
-                        elif 1 < malicious_count < 11:
-                            embed.title = "Analysis complete"
-                            embed.description = f"**{int(percent)}%** of vendors rated this file dangerous. While there are malicious ratings available for this file, there aren't many, so this could be a false positive. **You should investigate further before coming to a decision.**"
-                            embed.color = discord.Colour(0xff9144)
-                            embed.set_footer(text=f"SHA1 | {sha1}")
+                        if ctx.channel.permissions_for(ctx.guild.me).embed_links:
+                            embed = discord.Embed()
+                            content = f"||<@{presid}>||"
+                            if malicious_count >= 11:
+                                embed.title = "Analysis complete"
+                                embed.description = f"**{int(percent)}%** of vendors rated this file dangerous! You should avoid this file completely, and delete it from your systems to ensure security."
+                                embed.color = discord.Colour(0xff4545)
+                                embed.set_footer(text=f"SHA1 | {sha1}")
+                            elif 1 < malicious_count < 11:
+                                embed.title = "Analysis complete"
+                                embed.description = f"**{int(percent)}%** of vendors rated this file dangerous. While there are malicious ratings available for this file, there aren't many, so this could be a false positive. **You should investigate further before coming to a decision.**"
+                                embed.color = discord.Colour(0xff9144)
+                                embed.set_footer(text=f"SHA1 | {sha1}")
+                            else:
+                                embed.title = "Analysis complete"
+                                embed.color = discord.Colour(0x2BBD8E)
+                                embed.description = f"**{safe_count}** vendors say this file is malware-free"
+                                embed.set_footer(text=f"{sha1}")
+                            button = discord.ui.Button(label="View results on VirusTotal", url=f"https://www.virustotal.com/gui/file/{sha256}", style=discord.ButtonStyle.url)
+                            button2 = discord.ui.Button(label="Get a second opinion", url="https://discord.gg/6PbaH6AfvF", style=discord.ButtonStyle.url)
+                            view = discord.ui.View()
+                            view.add_item(button)
+                            view.add_item(button2)
+                            await ctx.send(content=content, embed=embed, view=view)
                         else:
-                            embed.title = "Analysis complete"
-                            embed.color = discord.Colour(0x2BBD8E)
-                            embed.description = f"**{safe_count}** vendors say this file is malware-free"
-                            embed.set_footer(text=f"{sha1}")
-                        button = discord.ui.Button(label="View results on VirusTotal", url=f"https://www.virustotal.com/gui/file/{sha256}", style=discord.ButtonStyle.url)
-                        button2 = discord.ui.Button(label="Get a second opinion", url="https://discord.gg/6PbaH6AfvF", style=discord.ButtonStyle.url)
-                        view = discord.ui.View()
-                        view.add_item(button)
-                        view.add_item(button2)
-                        await ctx.send(content=content, embed=embed, view=view)
+                            content = f"||<@{presid}>||"
+                            if malicious_count >= 11:
+                                await ctx.send(f"{content}\nAnalysis complete: **{int(percent)}%** of vendors rated this file dangerous! You should avoid this file completely, and delete it from your systems to ensure security.\nSHA1: {sha1}\nView results on VirusTotal: https://www.virustotal.com/gui/file/{sha256}\nGet a second opinion: https://discord.gg/6PbaH6AfvF")
+                            elif 1 < malicious_count < 11:
+                                await ctx.send(f"{content}\nAnalysis complete: **{int(percent)}%** of vendors rated this file dangerous. While there are malicious ratings available for this file, there aren't many, so this could be a false positive. **You should investigate further before coming to a decision.**\nSHA1: {sha1}\nView results on VirusTotal: https://www.virustotal.com/gui/file/{sha256}\nGet a second opinion: https://discord.gg/6PbaH6AfvF")
+                            else:
+                                await ctx.send(f"{content}\nAnalysis complete: **{safe_count}** vendors say this file is malware-free\nSHA1: {sha1}\nView results on VirusTotal: https://www.virustotal.com/gui/file/{sha256}\nGet a second opinion: https://discord.gg/6PbaH6AfvF")
                         self.log_submission(ctx.author.id, f"`{file_name}` - **{malicious_count}/{total_count}** - [View results](https://www.virustotal.com/gui/file/{sha256})")
                     else:
                         raise ValueError("Required hash values not found in the analysis response.")
             except (aiohttp.ClientResponseError, ValueError) as e:
-                embed = discord.Embed(title='Analysis failed', description=str(e), colour=discord.Colour.red())
-                embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
-                await ctx.send(embed=embed)
+                if ctx.channel.permissions_for(ctx.guild.me).embed_links:
+                    embed = discord.Embed(title='Analysis failed', description=str(e), colour=discord.Colour.red())
+                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
+                    await ctx.send(embed=embed)
+                else:
+                    await ctx.send(f"Analysis failed: {str(e)}")
             except asyncio.TimeoutError:
-                embed = discord.Embed(title='Request timed out', description="The bot was unable to complete the request due to a timeout.", colour=discord.Colour.red())
-                embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
-                await ctx.send(embed=embed)
+                if ctx.channel.permissions_for(ctx.guild.me).embed_links:
+                    embed = discord.Embed(title='Request timed out', description="The bot was unable to complete the request due to a timeout.", colour=discord.Colour.red())
+                    embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Red/close-circle-outline.png")
+                    await ctx.send(embed=embed)
+                else:
+                    await ctx.send("Request timed out. The bot was unable to complete the request due to a timeout.")
 
     def log_submission(self, user_id, summary):
         if user_id not in self.submission_history:
