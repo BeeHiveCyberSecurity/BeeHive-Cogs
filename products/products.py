31be603786d3e019e3a3ecb7b1e213b541940b7d
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-08-28 00:50:18 -0600

    Tests

--- products/products.py
+++ products/products.py
@@ -345,7 +345,7 @@ class Products(commands.Cog):
             description="Here's the applicable license information about Sentri.",
             colour=16767334
         )
-        embed.add_field(name="Built on Red", value="Sentri is built on Red, a modern and open-source Python framework for Discord bots. [Learn more about Red](https://discord.red)")
-        embed.add_field(name="Licensed under GPLv3", value="Red is a free and open source application made available to the public and licensed under the GNU GPLv3. [Read the license](https://github.com/Cog-Creators/Red-DiscordBot/blob/V3/develop/LICENSE)")
-        embed.add_field(name="Enhanced by BeeHive", value="This bot has been enhanced by BeeHive to provide additional features and functionality. [Learn more about Sentri](https://www.beehive.systems/sentri)")
+        embed.add_field(name="Built on Red", value="Sentri is built on Red, a modern and open-source Python framework for Discord bots. [Learn more about Red](https://discord.red)", inline=False)
+        embed.add_field(name="Licensed under GPLv3", value="Red is a free and open source application made available to the public and licensed under the GNU GPLv3. [Read the license](https://github.com/Cog-Creators/Red-DiscordBot/blob/V3/develop/LICENSE)", inline=False)
+        embed.add_field(name="Enhanced by BeeHive", value="This bot has been enhanced by BeeHive to provide additional features and functionality. [Learn more about Sentri](https://www.beehive.systems/sentri)", inline=False)
         await ctx.send(embed=embed)
