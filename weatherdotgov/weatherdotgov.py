46ac4e401d49e274656ab12c0713b70b3cb1a653
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-08-30 19:32:32 -0600

    Tweaks

--- weatherdotgov/weatherdotgov.py
+++ weatherdotgov/weatherdotgov.py
@@ -349,12 +349,11 @@ class Weather(commands.Cog):
                 embed.add_field(name="Reflectivity Calibration Correction", value=f"{reflectivity_calibration.get('value', 'Unknown')} {unit_code}", inline=True)
             
             if latency != "Unknown":
-                embed.add_field(name="Current Latency", value=f"{current_value}ms", inline=True)
-                embed.add_field(name="Average Latency", value=f"{average_value}ms", inline=True)
-                embed.add_field(name="Max Latency", value=f"{max_value}ms", inline=True)
+                embed.add_field(name="Current Latency", value=f"{current_value} ms", inline=True)
+                embed.add_field(name="Average Latency", value=f"{average_value} ms", inline=True)
+                embed.add_field(name="Max Latency", value=f"{max_value} ms", inline=True)
                 embed.add_field(name="L2 Last Received Time", value=level_two_last_received_time, inline=True)
                 embed.add_field(name="Max Latency Time", value=max_latency_time, inline=True)
-                embed.add_field(name="Reporting Host", value=reporting_host, inline=True)
                 embed.add_field(name="Host", value=host, inline=True)
             
             pages.append(embed)
