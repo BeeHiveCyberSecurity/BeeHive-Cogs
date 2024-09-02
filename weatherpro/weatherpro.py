40609d882018a801013e09c6dc76e1c07061b579
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-09-01 18:03:03 -0600

    update weatherpro.py

--- weatherpro/weatherpro.py
+++ weatherpro/weatherpro.py
@@ -64,6 +64,9 @@ class Weather(commands.Cog):
         result = round((f - 32) * 5.0 / 9.0, 1)
         return f"{result:.1f}"
     
+    def mph_to_knots(self, mph):
+        knots = mph * 0.868976
+        return round(knots, 2)
     
     @commands.group()
     async def weather(self, ctx):
@@ -219,7 +222,9 @@ class Weather(commands.Cog):
             embed.add_field(name="Pressure (MSL)", value=f"{current.get('pressure_msl', 'N/A')} hPa")
             embed.add_field(name="Surface pressure", value=f"{current.get('surface_pressure', 'N/A')} hPa")
             wind_speed = current.get('wind_speed_10m', 'N/A')
-            embed.add_field(name="Wind speed", value=f"{wind_speed} mph")
+            if wind_speed != 'N/A':
+                wind_speed_knots = self.mph_to_knots(wind_speed)
+                embed.add_field(name="Wind speed", value=f"{wind_speed} mph • {wind_speed_knots:.2f} knots")
             wind_direction = current.get('wind_direction_10m', 'N/A')
             if wind_direction != 'N/A':
                 if (wind_direction >= 0 and wind_direction <= 22.5) or (wind_direction > 337.5 and wind_direction <= 360):
