2b8930d621d076f85302525d59943adbf3855e5f
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-09-16 09:02:08 -0600

    update abuseipdb.py

--- abuseipdb/abuseipdb.py
+++ abuseipdb/abuseipdb.py
@@ -26,7 +26,7 @@ class AbuseIPDB(commands.Cog):
     @commands.group(name="abuseipdb")
     async def abuseipdb(self, ctx):
         """
-        AbuseIPDB is a project dedicated to helping combat the spread of hackers, spammers, and abusive activity on the internet by providing a central blacklist for webmasters, system administrators, and other interested parties to report and find IP addresses that have been associated with malicious activity online.
+        AbuseIPDB is a project dedicated to helping combat the spread of hackers, spammers, and abusive activity on the internet by providing a central blacklist for webmasters, system administrators, and other interested parties to report IP's engaged in bad behavior.
 
         Learn more at https://www.abuseipdb.com
         """
