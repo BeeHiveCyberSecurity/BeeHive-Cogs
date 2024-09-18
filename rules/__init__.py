711a46552e3f8a4660602194a92106597bf00959
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-09-18 05:12:44 -0600

    update rules/__init__.py and rules.py

--- rules/__init__.py
+++ rules/__init__.py
@@ -1,9 +1,4 @@
 from .rules import RulesCog
-from redbot.core import commands
-
-class RulesCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
 
 async def setup(bot):
     await bot.add_cog(RulesCog(bot))
