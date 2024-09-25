618912f0ce23941374ef83910a235720becd55c8
BeeHive <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-09-25 06:54:24 -0600

    update skysearch.py

--- skysearch/skysearch.py
+++ skysearch/skysearch.py
@@ -314,8 +314,9 @@ class Skysearch(commands.Cog):
                 "aircraft": {"name": "Aircraft tracked right now", "value": "`{} aircraft`"}
             }
 
-            embed = discord.Embed(title="Air traffic data", description="Live flight data powered by [airplanes.live](https://airplanes.live) API", color=0xfffffe)
+            embed = discord.Embed(title="SkySearch Statistics", description="Consolidated statistics and data sources for SkySearch.", color=0xfffffe)
             embed.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/White/airplane.png")
+
             for key, field in stats_fields.items():
                 if key in data:
                     name = field["name"]
@@ -324,46 +325,23 @@ class Skysearch(commands.Cog):
 
             embed.add_field(name="Appears in", value="`aircraft callsign` `aircraft icao` `aircraft reg` `aircraft squawk` `aircraft type` `aircraft radius` `aircraft pia` `aircraft mil` `aircraft ladd` `aircraft export`", inline=False)
 
-            embed2 = discord.Embed(title="Aircraft tags", description="Aircraft and air asset intelligence tags are powered by [BeeHive](https://www.beehive.systems). From time to time, you'll see tags on aircraft you query that contain cool or useful bits of information about them.", color=0xffd966)
-            embed2.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/Yellow/sparkles.png")
-            embed2.add_field(name="Law enforcement aircraft", value="`{:,} tagged`".format(len(self.law_enforcement_icao_set)), inline=True)
-            embed2.add_field(name="Military & government aircraft", value="`{:,} tagged`".format(len(self.military_icao_set)), inline=True)
-            embed2.add_field(name="Medical aircraft", value="`{:,} tagged`".format(len(self.medical_icao_set)), inline=True)
-            embed2.add_field(name="Media aircraft", value="`{:,} known`".format(len(self.newsagency_icao_set)), inline=True)
-            embed2.add_field(name="Damaged aircraft", value="`{:,} known`".format(len(self.global_prior_known_accident_set)), inline=True)
-            embed2.add_field(name="Wartime aircraft", value="`{:,} observed`".format(len(self.ukr_conflict_set)), inline=True)
-            embed2.add_field(name="Utility aircraft", value="`{:,} spotted`".format(len(self.agri_utility_set)), inline=True)
-            embed2.add_field(name="Balloons", value="`{:,} known`".format(len(self.balloons_icao_set)), inline=True)
-            embed2.add_field(name="Suspicious aircraft", value="`{:,} identifiers`".format(len(self.suspicious_icao_set)), inline=True)
-            embed2.add_field(name="Appears in", value="`aircraft callsign` `aircraft icao` `aircraft reg` `aircraft squawk` `aircraft type` `aircraft radius` `aircraft pia` `aircraft mil` `aircraft ladd`", inline=False)
+            embed.add_field(name="Law enforcement aircraft", value="`{:,} tagged`".format(len(self.law_enforcement_icao_set)), inline=True)
+            embed.add_field(name="Military & government aircraft", value="`{:,} tagged`".format(len(self.military_icao_set)), inline=True)
+            embed.add_field(name="Medical aircraft", value="`{:,} tagged`".format(len(self.medical_icao_set)), inline=True)
+            embed.add_field(name="Media aircraft", value="`{:,} known`".format(len(self.newsagency_icao_set)), inline=True)
+            embed.add_field(name="Damaged aircraft", value="`{:,} known`".format(len(self.global_prior_known_accident_set)), inline=True)
+            embed.add_field(name="Wartime aircraft", value="`{:,} observed`".format(len(self.ukr_conflict_set)), inline=True)
+            embed.add_field(name="Utility aircraft", value="`{:,} spotted`".format(len(self.agri_utility_set)), inline=True)
+            embed.add_field(name="Balloons", value="`{:,} known`".format(len(self.balloons_icao_set)), inline=True)
+            embed.add_field(name="Suspicious aircraft", value="`{:,} identifiers`".format(len(self.suspicious_icao_set)), inline=True)
+            embed.add_field(name="Appears in", value="`aircraft callsign` `aircraft icao` `aircraft reg` `aircraft squawk` `aircraft type` `aircraft radius` `aircraft pia` `aircraft mil` `aircraft ladd`", inline=False)
 
-            embed3 = discord.Embed(title="Photography", description="Photos are powered by community contributions at [planespotters.net](https://www.planespotters.net/)", color=0xfffffe)
-            embed3.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/White/camera.png")
-            embed3.add_field(name="Appears in", value="`aircraft callsign` `aircraft icao` `aircraft reg` `aircraft squawk` `aircraft type`", inline=False)
-
-            embed4 = discord.Embed(title="Airport data", description="Airport data is powered by the [airport-data.com](https://airport-data.com/) API service", color=0xfffffe)
-            embed4.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/White/location.png")
-            embed4.add_field(name="Appears in", value="`airport about`", inline=False)
-
-            embed5 = discord.Embed(title="Runway data", description="Runway data is powered by the [airportdb.io](https://airportdb.io) API service", color=0xfffffe)
-            embed5.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/White/layers.png")
-            embed5.add_field(name="Appears in", value="`airport runway`", inline=False)
-
-            embed6 = discord.Embed(title="Mapping and imagery", description="Mapping and ground imagery powered by [Google Maps](https://maps.google.com) and the [Maps Static API](https://developers.google.com/maps/documentation/maps-static)", color=0xfffffe)
-            embed6.set_thumbnail(url="https://www.beehive.systems/hubfs/Icon%20Packs/White/map.png")
-            embed6.add_field(name="Appears in", value="`airport about`", inline=False)
+            embed.add_field(name="Photography", value="Photos are powered by community contributions at [planespotters.net](https://www.planespotters.net/)", inline=False)
+            embed.add_field(name="Airport data", value="Airport data is powered by the [airport-data.com](https://airport-data.com/) API service", inline=False)
+            embed.add_field(name="Runway data", value="Runway data is powered by the [airportdb.io](https://airportdb.io) API service", inline=False)
+            embed.add_field(name="Mapping and imagery", value="Mapping and ground imagery powered by [Google Maps](https://maps.google.com) and the [Maps Static API](https://developers.google.com/maps/documentation/maps-static)", inline=False)
 
             await ctx.send(embed=embed)
-            await asyncio.sleep(1)  # Short wait for rate limiting
-            await ctx.send(embed=embed2)
-            await asyncio.sleep(1)  # Short wait for rate limiting
-            await ctx.send(embed=embed3)
-            await asyncio.sleep(1)  # Short wait for rate limiting
-            await ctx.send(embed=embed4)
-            await asyncio.sleep(1)  # Short wait for rate limiting
-            await ctx.send(embed=embed5)
-            await asyncio.sleep(1)  # Short wait for rate limiting
-            await ctx.send(embed=embed6)
         except aiohttp.ClientError as e:
             embed = discord.Embed(title="Error", description=f"Error fetching data: {e}", color=0xff4545)
             await ctx.send(embed=embed)
