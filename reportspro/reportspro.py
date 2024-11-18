a59bbaf8db9c30f42ddbb73738aa34842a5509fe
BeeHive <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-11-18 02:36:52 -0700

    update reportspro.py

--- reportspro/reportspro.py
+++ reportspro/reportspro.py
@@ -187,10 +187,10 @@ class ReportsPro(commands.Cog):
                     report_message.add_field(name="Report ID", value=f"```{report_id}```", inline=False)
                     report_message.add_field(name="Offender", value=f"{self.member.mention}", inline=True)
                     report_message.add_field(name="Offender ID", value=f"`{self.member.id}`", inline=True)
+                    report_message.add_field(name="Date", value=f"<t:{int(datetime.now(timezone.utc).timestamp())}:D>", inline=True)
                     report_message.add_field(name="Reporter", value=self.ctx.author.mention, inline=True)
                     report_message.add_field(name="Reporter ID", value=f"`{self.ctx.author.id}`", inline=True)
                     report_message.add_field(name="Time", value=f"<t:{int(datetime.now(timezone.utc).timestamp())}:R>", inline=True)
-                    report_message.add_field(name="Date", value=f"<t:{int(datetime.now(timezone.utc).timestamp())}:D>", inline=True)
                     report_message.add_field(name="Reason", value=f"**{selected_reason}**\n*{selected_description}*", inline=False)
                     
                     # Add a summary of existing report counts by reason
