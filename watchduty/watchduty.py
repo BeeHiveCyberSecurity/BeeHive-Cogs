700964a2bccfcc4b70b69a6cd9ce4465172f2aff
BeeHive <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2024-11-28 00:54:05 -0700

    update watchduty.py

--- watchduty/watchduty.py
+++ watchduty/watchduty.py
@@ -14,31 +14,51 @@ class WatchDuty(commands.Cog):
     async def disable_mass_mentions(self, ctx):
         """Disable the ability to use mass mentions in the server."""
         roles_modified = 0
+        roles_skipped = 0
         for guild in self.bot.guilds:
             for role in guild.roles:
                 if guild.me.top_role > role:  # Check if the bot has permission to edit the role
-                    # Deny the permission to mention everyone, here, and roles
-                    new_permissions = role.permissions
-                    new_permissions.update(mention_everyone=False)
-                    await role.edit(permissions=new_permissions)
-                    roles_modified += 1
+                    try:
+                        # Deny the permission to mention everyone, here, and roles
+                        new_permissions = role.permissions
+                        new_permissions.update(mention_everyone=False)
+                        await role.edit(permissions=new_permissions)
+                        roles_modified += 1
+                    except discord.errors.Forbidden:
+                        roles_skipped += 1
+                        continue
 
-        await ctx.send(f"Mass mentions have been disabled for {roles_modified} roles.")
+        embed = discord.Embed(
+            title="Mass Mentions Disabled",
+            description=f"Mass mentions have been disabled for {roles_modified} roles. {roles_skipped} roles could not be modified due to missing permissions.",
+            color=discord.Color.red()
+        )
+        await ctx.send(embed=embed)
 
     @watchduty_group.command(name="externalapps")
     async def disable_external_apps(self, ctx):
         """Disable the ability to use external apps in the server."""
         roles_modified = 0
+        roles_skipped = 0
         for guild in self.bot.guilds:
             for role in guild.roles:
                 if guild.me.top_role > role:  # Check if the bot has permission to edit the role
-                    # Deny the permission to use external apps
-                    new_permissions = role.permissions
-                    new_permissions.update(use_external_apps=False)
-                    await role.edit(permissions=new_permissions)
-                    roles_modified += 1
+                    try:
+                        # Deny the permission to use external apps
+                        new_permissions = role.permissions
+                        new_permissions.update(use_external_apps=False)
+                        await role.edit(permissions=new_permissions)
+                        roles_modified += 1
+                    except discord.errors.Forbidden:
+                        roles_skipped += 1
+                        continue
 
-        await ctx.send(f"External apps have been disabled for {roles_modified} roles.")
+        embed = discord.Embed(
+            title="External Apps Disabled",
+            description=f"External apps have been disabled for {roles_modified} roles. {roles_skipped} roles could not be modified due to missing permissions.",
+            color=discord.Color.red()
+        )
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(WatchDuty(bot))
