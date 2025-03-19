707c2678ff51b907d9a1988f3531e8a08922c82c
BeeHive <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2025-03-19 01:40:58 -0600

    delete 6 files

--- vanityguard/__init__.py
+++
@@ -1,4 +0,0 @@
-from .vanityguard import VanityGuard
-
-async def setup(bot):
-    await bot.add_cog(VanityGuard(bot))
--- watchduty/__init__.py
+++
@@ -1,5 +0,0 @@
-from .watchduty import WatchDuty
-
-async def setup(bot):
-    cog = WatchDuty(bot)
-    await bot.add_cog(cog)
