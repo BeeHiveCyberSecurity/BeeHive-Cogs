80e4456213d51338af2327221f39109001ed17de
BeeHive <jtrasiti@beehive.systems>
2025-04-04 11:36:52 -0600

    update cloudflare.py

--- cloudflare/cloudflare.py
+++ cloudflare/cloudflare.py
@@ -1263,7 +1263,7 @@ class Cloudflare(commands.Cog):
                     "clienttransferprohibited": ":lock: **Transfer prohibited**",
                     "clientdeleteprohibited": ":no_entry: **Deletion prohibited**",
                     "clientupdateprohibited": ":pencil2: **Update prohibited**",
-                    "clienthold": ":pause_button: **On hold**",
+                    "clienthold": ":pause_button: **Held by registrar**",
                     "servertransferprohibited": ":lock: **Server locked**",
                     "serverdeleteprohibited": ":no_entry: **Server deletion prohibited**",
                     "serverupdateprohibited": ":pencil2: **Server update prohibited**",
