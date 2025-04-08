65e8b21ac1d214d935e1c74977a690737467139f
BeeHive <jtrasiti@beehive.systems>
2025-04-08 01:58:24 -0600

    update summarizer.py

--- summarizer/summarizer.py
+++ summarizer/summarizer.py
@@ -88,7 +88,7 @@ class ChatSummary(commands.Cog):
             )
             if not customer_id:
                 embed.set_footer(text="You're using the free version of BeeHive's AI summarizer. Upgrade for improved speed, intelligence, and functionality.")
-            else
+            else:
                 embed.set_footer(text="You're powered up with premium AI models and extended discussion context.")
             await ctx.send(embed=embed)
 
