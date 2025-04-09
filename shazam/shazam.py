79bb78ce9a7a5faade59e70eb4a58d7e67b3fec5
BeeHive <jtrasiti@beehive.systems>
2025-04-09 03:26:39 -0600

    update shazam.py

--- shazam/shazam.py
+++ shazam/shazam.py
@@ -103,11 +103,14 @@ class ShazamCog(commands.Cog):
                     if hub_info.get('explicit', False):
                         embed.set_footer(text="Song contains explicit content, audience discretion advised")
 
-                    # Create URL buttons for Shazam and Apple Music
+                    # Create URL buttons for Shazam, Apple Music, and YouTube Music
                     view = discord.ui.View()
                     shazam_url = track.get('url', '')
                     apple_music_option = track.get('hub', {}).get('options', [{}])[0]
                     apple_music_url = apple_music_option.get('actions', [{}])[0].get('uri', '')
+
+                    youtube_music_option = track.get('hub', {}).get('providers', [{}])[1]
+                    youtube_music_url = youtube_music_option.get('actions', [{}])[0].get('uri', '')
 
                     if shazam_url:
                         shazam_button = discord.ui.Button(label="Listen on Shazam", url=shazam_url)
@@ -116,6 +119,10 @@ class ShazamCog(commands.Cog):
                     if apple_music_url:
                         apple_music_button = discord.ui.Button(label="Open in Apple Music", url=apple_music_url)
                         view.add_item(apple_music_button)
+
+                    if youtube_music_url:
+                        youtube_music_button = discord.ui.Button(label="Open in YouTube Music", url=youtube_music_url)
+                        view.add_item(youtube_music_button)
 
                     # Convert track_info to JSON and send as a file
                     json_data = json.dumps(track_info, indent=4)
